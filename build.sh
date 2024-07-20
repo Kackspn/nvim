dcaf4e1675c84aea39f9ad1ab71f79b484823ae3
Jackson Streeter <jpstreeter15@gmail.com>
2024-07-20 16:49:27 -0600

    added build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+
+sudo mkdir -p /opt/nvim
+sudo mv nvim.appimage /opt/nvim/nvim
+
+sudo apt install npm python3.10-venv luarocks clangd pip cargo ripgrep cmake -y
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+sudo mv zsh-syntax-highlighting /usr/share/zsh
+sudo mv zsh-autosuggestions /usr/share/zsh
