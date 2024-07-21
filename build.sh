8ebf2141ba48e42d7e39a8f5ae132c4cc0189825
Jackson Streeter <jpstreeter15@gmail.com>
2024-07-20 18:11:20 -0600

    build update

--- build.sh
+++ build.sh
@@ -2,14 +2,12 @@
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
 
 sudo mkdir -p /opt/nvim
 sudo mv nvim.appimage /opt/nvim/nvim
 
 sudo apt install zsh npm python3.10-venv luarocks clangd pip cargo ripgrep cmake -y
 
-cd nvim/
 mv .zshrc ~
 sudo chsh -s $(which zsh)
 cd ~
