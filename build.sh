45f02e319a5174e922945f5dcc480e25df49ade3
Kackspn <jpstreeter15@gmail.com>
2024-08-11 15:16:33 -0600

    new?

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ chmod u+x nvim.appimage
 sudo mkdir -p /opt/nvim
 sudo mv nvim.appimage /opt/nvim/nvim
 
-sudo apt install zsh npm python3.10-venv luarocks clangd pip cargo ripgrep cmake -y
+sudo apt install zsh npm python3.12-venv xclip luarocks clangd pip cargo ripgrep cmake -y
 
 mv .zshrc ~
-sudo chsh -s $(which zsh)
+zsh
+chsh -s $(which zsh)
 cd ~
 source .zshrc
 sudo mv nvim .config
@@ -18,3 +19,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 sudo mv zsh-syntax-highlighting /usr/share/zsh
 sudo mv zsh-autosuggestions /usr/share/zsh
+cd ~ && ln .zshrc .congif/nvim/.zshrc
