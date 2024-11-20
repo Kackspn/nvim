215c4e6d51c684702e50f41ae11ebb2c9e93c87e
Kackspn <jpstreeter15@gmail.com>
2024-11-19 18:26:23 -0700

    added neocodeium status to status line

--- lua/plugins/neocodeium.lua
+++ lua/plugins/neocodeium.lua
@@ -3,8 +3,28 @@ return {
   event = "VeryLazy",
   config = function()
     local neocodeium = require("neocodeium")
+    local lualine = require("lualine")
     neocodeium.setup()
-    vim.keymap.set("i", "<A-f>", neocodeium.accept)
+    lualine.setup({
+        sections = {
+          lualine_c = vim.list_extend(lualine.get_config().sections.lualine_c, {
+            function()
+              local status, _ = require("neocodeium").get_status()
+              local status_symbols = {
+                [0] = "󱜙  ON", -- Enabled
+                [1] = "󱚧  OFF", -- Disabled Globally
+                [2] = "󱚧  OFF", -- Disabled for Buffer (catch-all)
+                [3] = "󱚧  OFF", -- Disabled for Buffer filetype
+                [4] = "󱚧  OFF", -- Disabled Callback
+                [5] = "󱚧  OFF", -- Disabled for Buffer encoding
+              }
+
+              return status_symbols[status]
+            end,
+          }),
+        },
+      })
+
   end,
 }
 
